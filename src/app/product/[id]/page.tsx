379be058fd3ad82d38e8f364449b23d92aade129
SajidHussainabbasi <sajid_abbasi2000@yahoo.com>
2025-06-17 18:11:45 +0200

    Fix ProductPage props type

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -1,17 +1,18 @@
-
 import { fetchProductById } from '../../../apis/getProducts';
 import ProductDetails from '../../../components/ProductDetails';
 
-export default async function ProductPage({
-    params
-}: {
-    params: { id: string };
-}) {
-    const product = await fetchProductById(params.id);
+type ProductPageProps = {
+  params: {
+    id: string;
+  };
+};
 
-    return (
-        <main className="max-w-2xl mx-auto p-6">
-            <ProductDetails product={product} />
-        </main>
-    );
+export default async function ProductPage({ params }: ProductPageProps) {
+  const product = await fetchProductById(params.id);
+
+  return (
+    <main className="max-w-2xl mx-auto p-6">
+      <ProductDetails product={product} />
+    </main>
+  );
 }
